889461f4df30d39a24ddf79592a5d73db2165933
sbentley2019 <sbentley000@gmail.com>
2019-11-12 14:55:19 +0000

    Fix styling errors in javascript and add comments to functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
-
   $('button#compose-tweet').on('click', function() {
-    // Toggles the tweet box visibility
+    // Toggles the tweet box visibility.
     if ($('.new-tweet').css('display') === 'none') {
       $('.new-tweet').slideDown('slow');
       $("textarea").focus();
@@ -12,6 +11,7 @@ $(document).ready(function() {
   });
 
   const renderTweets = function(tweets) {
+    // A function that loops over the tweets array, calls createTweet and prepends it to the tweet container.
     tweets.forEach(function(element) {
       const $tweet = createTweetElement(element);
       $('#tweets-container').prepend($tweet);
@@ -19,6 +19,7 @@ $(document).ready(function() {
   };
   
   const timePosted = function(time) {
+    // A function that takes in a time in milliseconds and returns an array of time and time units.
     if (time / 1000 < 60) {
       return [Math.floor(time / 1000), 'seconds'];
     } else if (time / (1000 * 60) < 60) {
@@ -31,6 +32,7 @@ $(document).ready(function() {
   };
 
   const createTweetElement = function(tweetObj) {
+    // A function that creates and returns the html of the tweets using a tweet object.
     const time = timePosted(new Date().getTime() - new Date(tweetObj.created_at).getTime());
       
     const $tweet =
@@ -56,6 +58,7 @@ $(document).ready(function() {
   };
 
   const loadTweets = function() {
+    // ajax get request that uses renderTweets function to loads the tweets.
     $.ajax('/tweets', { method: 'GET' })
       .then(function(tweetPost) {
         renderTweets(tweetPost);
@@ -64,12 +67,14 @@ $(document).ready(function() {
   loadTweets();
 
   const escape = function(str) {
+    // escape str so there's no cross-site scripting, returns text.
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
   const isTweetValidated = function(tweet) {
+    // This function displays an error message/returns false if tweet is empty or length is > 140 else it returns true. 
     if (!tweet.value) {
       $('#error-message span').text(' Tweet has to have at least one character. ');
       $('#error-message').slideDown('slow');
@@ -84,23 +89,29 @@ $(document).ready(function() {
     }
   };
 
+  const postAjax = function(obj) {
+    // This function does an ajax post request separate from form submit logic.
+    $.ajax({
+      url: '/tweets',
+      type: 'POST',
+      data: $(obj).serialize()
+    }).then(() => {
+      $.ajax('/tweets', { method: 'GET'})
+        .then(function(tweetPost) {
+          renderTweets([tweetPost[tweetPost.length - 1]]);
+        });
+    });
+  }
+
   $('#form-tweet').submit(function(e) {
+    // This catches the submit on the tweet post, prevents submit default, escapes cross-site scripting a gets ajax request.
     e.preventDefault();
     $('#error-message').slideUp('slow');
     $(this)[0][0].value = escape($(this)[0][0].value);
     if (isTweetValidated($(this)[0][0])) {
-      $.ajax({
-        url: '/tweets',
-        type: 'POST',
-        data: $(this).serialize()
-      }).then(() => {
-        $.ajax('/tweets', { method: 'GET'})
-          .then(function(tweetPost) {
-            renderTweets([tweetPost[tweetPost.length - 1]]);
-          });
-      });
+      postAjax(this);
       $('form textarea').val('');
-      $('.counter').text('140');
+      $('.counter').text('140').css('color', '#545149');
     }
   });
 });
